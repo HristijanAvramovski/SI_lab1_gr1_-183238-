8aab878590bf10357537acaf97ace09defd028a8
HristijanAvramovski <havramovski@gmail.com>
2020-04-07 21:18:27 +0200

    student class finished

--- Student.java
+++ Student.java
@@ -4,9 +4,37 @@ class Student {
 	String lastName;
         List<int> labPoints=new ArrayList<int>();
 
-	//TODO constructor
+	public Student(String index,String firstName,String lastName)
+        {
+           this.index=index;
+           this.firstName=firstName;
+           this.lastName=lastName;
+        }
 
-	//TODO seters & getters
+	public String getIndex()
+        {
+           return index;
+        }
+        public String getFirstName()
+        {
+           return firstName;
+        }
+        public String getLastName()
+        {
+           reutrn lastName;
+        }
+        public void setIndex(String index)
+        {
+           this.index=index;
+        }
+        public void setFirstName(String firstName)
+        {
+           this.firstName=firstName;
+        }
+        public void setLastName(String lastName)
+        {
+           this.lastName=lastName;
+        }
 
 	public double getAverage() {
 		//TODO
